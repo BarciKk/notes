799b4c92114ee0954417915f311318cd787ff9d2
Barcik <kamil.bartuzel2@o2.pl>
2023-07-01 23:43:13 +0200

    added data object as base for tomorow

---
+++ helpers.js
@@ -0,0 +1,21 @@
+const formatDate = () => {
+  const date = new Date();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let hours = date.getHours();
+  let min = date.getMinutes();
+  let day = days[date.getDay()];
+
+  let formatedHours = hours < 10 ? `0${hours}` : hours;
+  let formatedMin = min < 10 ? `0${min}` : min;
+  let formatedDay = day < 10 ? `0${day}` : day;
+
+  return `${formatedDay} ${formatedHours}:${formatedMin}`;
+};
--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-const addNewNote = document.querySelectorAll(".add-note-button");
+const addNewNoteButton = document.querySelectorAll(".add-note-button");
 const modal = document.querySelector(".modal");
 const backArrowModal = document.querySelector(".arrow-back-modal");
 const saveNote = document.querySelector(".save-note");
+const noteTitle = document.querySelector(".title-note");
+const noteValue = document.querySelector(".note-text-holder");
+
+let noteData = [];
+
+const addNewNote = () => {
+  const formatedDate = formatDate();
+
+  const data = {
+    id: Math.floor(Math.random() * 100),
+    title: noteTitle.value,
+    value: noteValue.value,
+    date: formatedDate,
+  };
+  noteData.push(data);
+};
 
 const toogleModal = () => {
   modal.style.display = modal.style.display === "block" ? "none" : "block";
 };
 
-addNewNote.forEach((button) => {
+saveNote.addEventListener("click", addNewNote);
+addNewNoteButton.forEach((button) => {
   button.addEventListener("click", toogleModal);
 });
 backArrowModal.addEventListener("click", toogleModal);
