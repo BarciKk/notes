5a2fbe7e23efc75cd6ad62a272339208a74a7c25
Barcik <kamil.bartuzel2@o2.pl>
2023-07-02 17:10:59 +0200

    i added image on the front and rendered notes on the screen

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const addNewNoteButton = document.querySelectorAll(".add-note-button");
 const modal = document.querySelector(".modal");
 const backArrowModal = document.querySelector(".arrow-back-modal");
+const noteContainer = document.querySelector(".greeting-stuff");
 const saveNote = document.querySelector(".save-note");
 const noteTitle = document.querySelector(".title-note");
 const noteValue = document.querySelector(".note-text-holder");
@@ -8,8 +9,8 @@ const noteValue = document.querySelector(".note-text-holder");
 let noteData = [];
 
 const addNewNote = () => {
+  if (noteTitle.value === "") return;
   const formatedDate = formatDate();
-
   const data = {
     id: Math.floor(Math.random() * 100),
     title: noteTitle.value,
@@ -17,6 +18,32 @@ const addNewNote = () => {
     date: formatedDate,
   };
   noteData.push(data);
+
+  renderNotes();
+  noteTitle.value = "";
+  noteValue.value = "";
+};
+console.log(noteData);
+const renderNotes = () => {
+  noteContainer.textContent = "";
+
+  noteData.forEach(({ title, date }) => {
+    const noteDiv = document.createElement("div");
+    const titleElement = document.createElement("p");
+    const dateElement = document.createElement("p");
+
+    noteDiv.className = "note-con";
+    titleElement.className = "note-title-container";
+    dateElement.className = "note-date-container";
+
+    titleElement.textContent = title;
+    dateElement.textContent = date;
+
+    noteDiv.appendChild(titleElement);
+    noteDiv.appendChild(dateElement);
+    noteContainer.appendChild(noteDiv);
+  });
+  toogleModal();
 };
 
 const toogleModal = () => {
