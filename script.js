629a0b27d039be0f7d2782bf17c5ed40d560b791
Barcik <kamil.bartuzel2@o2.pl>
2023-07-20 19:34:20 +0200

    I added note preview modal changed logic of deleting notes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const addNewNoteButton = document.querySelectorAll(".add-note-button");
 const modal = document.querySelector(".modal");
-const backArrowModal = document.querySelector(".arrow-back-modal");
+const backArrowModal = document.querySelectorAll(".arrow-back-modal");
 const note = document.querySelector(".note");
 const noteContainer = document.querySelector(".note");
 const saveNote = document.querySelector(".save-note");
 const noteTitle = document.querySelector(".title-note");
 const noteValue = document.querySelector(".note-text-holder");
+const noteCreatorModal = document.querySelector(".note-creator");
+const displayCreatedNoteModal = document.querySelector(".show-created-note");
 
 let noteData = [];
+let holdTimer;
 
 const addNewNote = () => {
   if (noteTitle.value === "") return;
@@ -37,16 +40,25 @@ const renderNotes = () => {
     titleElement.className = "note-title-container";
     dateElement.className = "note-date-container";
 
+    noteDiv.dataset.noteId = id;
     titleElement.textContent = title;
     dateElement.textContent = date;
 
     noteDiv.appendChild(titleElement);
     noteDiv.appendChild(dateElement);
-    noteDiv.addEventListener("dblclick", () => deleteNote(id));
+    noteDiv.addEventListener("mousedown", () => {
+      holdTimer = setTimeout(() => {
+        deleteNote(id);
+      }, 300);
+    });
+
+    noteDiv.addEventListener("mouseup", () => {
+      clearTimeout(holdTimer);
+    });
 
     noteContainer.appendChild(noteDiv);
   });
-  toogleModal();
+  toogleCreatorModal();
 };
 
 const saveToLocalStorage = () => {
@@ -58,7 +70,7 @@ const loadFromLocalStorage = () => {
     noteData = JSON.parse(storedLocalData);
     if (noteData.length > 0) {
       renderNotes();
-      toogleModal();
+      toogleCreatorModal();
     } else {
       localStorage.removeItem("data");
     }
@@ -74,13 +86,57 @@ const deleteNote = (id) => {
 
   saveToLocalStorage();
 };
-const toogleModal = () => {
+
+const toogleCreatorModal = () => {
   modal.style.display = modal.style.display === "block" ? "none" : "block";
+  if (modal.style.display === "block") {
+    noteCreatorModal.style.display = "block";
+    displayCreatedNoteModal.style.display = "none";
+  } else {
+    noteCreatorModal.style.display = "none";
+    displayCreatedNoteModal.style.display = "none";
+  }
 };
 
-saveNote.addEventListener("click", addNewNote);
-addNewNoteButton.forEach((button) => {
-  button.addEventListener("click", toogleModal);
+const toogleDisplayCreatedNoteModal = (id) => {
+  modal.style.display = modal.style.display === "block" ? "none" : "block";
+
+  if (modal.style.display === "block") {
+    noteCreatorModal.style.display = "none";
+    displayCreatedNoteModal.style.display = "block";
+
+    const titleInput = document.getElementById("title-input");
+    const textAreaDescription = document.getElementById("note-description");
+
+    const clickedNote = noteData.find((note) => note.id === id);
+    if (clickedNote) {
+      const { title, value } = clickedNote;
+      titleInput.value = title;
+      textAreaDescription.value = value;
+    }
+  } else {
+    displayCreatedNoteModal.style.display = "none";
+  }
+};
+
+noteContainer.addEventListener("click", (event) => {
+  const clickedNote = event.target.closest(".note-container");
+  if (clickedNote) {
+    const id = parseInt(clickedNote.dataset.noteId, 10);
+    toogleDisplayCreatedNoteModal(id);
+  }
 });
-backArrowModal.addEventListener("click", toogleModal);
+
+saveNote.addEventListener("click", addNewNote);
+
+addNewNoteButton.forEach((button) => {
+  button.addEventListener("click", toogleCreatorModal);
+});
+
+backArrowModal.forEach((e) => {
+  e.addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+});
+
 window.addEventListener("load", loadFromLocalStorage);
