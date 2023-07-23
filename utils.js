457c35d132cdedf6e77e89055c6309397f448d19
Barcik <kamil.bartuzel2@o2.pl>
2023-07-23 19:46:22 +0200

    trying fix issue

---
+++ utils.js
@@ -0,0 +1,13 @@
+const HOLD_TIMER_DURATION = 300;
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const errorMessage = "You have to add at least title !";
+
+export { HOLD_TIMER_DURATION, days, errorMessage };
